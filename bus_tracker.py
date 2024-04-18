2fdadfab8d9f564226dae8549b1340facdd7e13c
Mesut <mesutahmettravaci@gmail.com>
2024-04-18 17:59:03 +0300

    Update bus_tracker.py

--- bus_tracker.py
+++ bus_tracker.py
@@ -1,12 +1,11 @@
 import zeep
 import json
 import re as r
-from pprint import pprint
 
 
 def announcements(line_code):
     url = 'https://api.ibb.gov.tr/iett/UlasimDinamikVeri/Duyurular.asmx?wsdl'
-    client = zeep.Client(wsdl = url)
+    client = zeep.Client(wsdl=url)
     anns = json.loads(client.service.GetDuyurular_json())
     l = list()
     a = 0
@@ -16,15 +15,17 @@ def announcements(line_code):
             l.append(i['MESAJ'])
     return a,l
 
+
 def stopping_buses():
     url = 'https://api.ibb.gov.tr/iett/FiloDurum/SeferGerceklesme.asmx?wsdl'
-    client = zeep.Client(wsdl = url)
+    client = zeep.Client(wsdl=url)
     anns = json.loads(client.service.GetFiloAracKonum_json())
     l = list()
     for i in anns:
         if i['Hiz'] == '0':
             l.append(i['KapiNo'])
     return l
+
 
 def max_speeds():
     url = 'https://api.ibb.gov.tr/iett/FiloDurum/SeferGerceklesme.asmx?wsdl'
@@ -58,6 +59,7 @@ def show_line_stops(line_code, direction):
             l.append(i.find('DURAKADI').text)
     return l
 
+
 def live_tracking(line_code, direction):
     urlstop = 'https://api.ibb.gov.tr/iett/ibb/ibb.asmx?wsdl'
     urlbus = 'https://api.ibb.gov.tr/iett/FiloDurum/SeferGerceklesme.asmx?wsdl'
@@ -66,26 +68,29 @@ def live_tracking(line_code, direction):
     clientbus = zeep.Client(wsdl=urlbus)
 
     stoploc_xml = clientstop.service.DurakDetay_GYY(line_code)
-    stops_locs = list()
 
-    for i in stoploc_xml.findall('Table'):
-        l = list()
-        if i.find('YON').text == direction:
-            l.append(i.find('DURAKADI').text)
-            l.append(float(i.find('YKOORDINATI').text))
-            l.append(float(i.find('XKOORDINATI').text))
-            stops_locs.append(l)
+    stops_locs = []
+    for table in stoploc_xml.getchildren():
+        l = []
+        l.append(table.find('DURAKADI').text)
+        l.append(float(table.find('YKOORDINATI').text))
+        l.append(float(table.find('XKOORDINATI').text))
+        stops_locs.append(l)
 
     busloc = json.loads(clientbus.service.GetHatOtoKonum_json(line_code))
-    l = list()
-    for i in busloc:
-        match = r.search(r'_(\w)_', i['guzergahkodu']).group()
-        if match == '_' + direction + '_':
-            l.append([i['kapino'],float(i['enlem']),float(i['boylam'])])
-    print(stops_locs)
-    with open("where.js", "w") as file:
-        file.write(f'stops = {stops_locs}\nbuses = {l}')
 
-def main():
+    bus_locs = []
+    for i in busloc:
+        match = r.search(r'_(\w)_', i['guzergahkodu']).group(1)
+        l = []
+        if match == direction:
+            l.append(i['kapino'])
+            l.append(float(i['enlem']))
+            l.append(float(i['boylam']))
+            bus_locs.append(l)
+
+    with open("where.js", "w") as file:
+        file.write(f'stops = {json.dumps(stops_locs)};\nbuses = {json.dumps(bus_locs)};')
+
     return 0
-main()
+
